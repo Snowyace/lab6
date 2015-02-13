6fa417e205d766b42cf0c4b51df16dec37fdd52b
Allan Asis <aasis@ucsd.edu>
2015-02-13 02:31:06 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,13 +20,27 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get("/project/" + idNumber, rustler);
 	console.log("User clicked on project " + idNumber);
+	
+
+}
+
+function rustler(result){
+	console.log(result);
+
+	var projectHTML = '<a href="#" class="thumbnail" +>' +
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' +  result['date'] +
+		'</small></p></a>' + '<p>' + result['summary'] + '</p>' ;
+
+	var proto = "#project" + result['id'];
+	$(proto + " .details" ).html(projectHTML);
 }
 
 /*
@@ -35,4 +49,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", colortest);
+}
+
+function colortest(result){
+	 var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+	 console.log(colors);
+
 }
